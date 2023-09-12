e4c02e0ee10c33454301d16868ba45585b64fc53
Jace B <jace.e.blackwell-1@ou.edu>
2023-09-12 16:27:25 -0500

    Finished step 2 and added main function

--- assignment3.cpp
+++ assignment3.cpp
@@ -12,3 +12,19 @@ int add(vector<int> first)
     }
     return total;
 }
+
+int multiply(vector<int> second)
+{ 
+    int total = second.at(0);
+    for (int i = 1; i < second.size(); ++i)
+    { 
+        total *= second.at(i);
+    }
+    return total;
+}
+
+int main()
+{ 
+
+    return 0;
+}
