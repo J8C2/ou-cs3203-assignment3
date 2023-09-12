e9209f66335e5681567249f5f51471e8b98ebe34
Jace B <jace.e.blackwell-1@ou.edu>
2023-09-12 16:51:59 -0500

    Finished step 5

--- assignment3.cpp
+++ assignment3.cpp
@@ -23,24 +23,41 @@ int multiply(vector<int> second)
     return total;
 }
 
+vector<int> reverse(vector<int> third)
+{ 
+    vector<int> reversal;
+    for (int i = third.size()-1; i >= 0; --i)
+    { 
+        reversal.at(third.at(i));
+    }
+    return reversal;
+}
+
 int main() // max of 5 values for each vector
 { 
     int MAX = 10;
     int temp = 0;
     vector<int> first;
     vector<int> second;
+    vector<int> third;
+
     for (int i = 0; i < MAX; ++i)
     { 
         cin >> temp;
         first.at(temp);
     }
-     for (int i = 0; i < MAX; ++i)
+    for (int i = 0; i < MAX; ++i)
     { 
         cin >> temp;
         second.at(temp);
     }
+    for (int i = 0; i < MAX; ++i)
+    { 
+        cin >> temp;
+        third.at(temp);
+    }
     
-    add(first);
-    multiply(second);
+    cout << add(first);
+    cout << multiply(second);
     return 0;
 }
