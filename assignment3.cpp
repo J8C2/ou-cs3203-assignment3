dddf4f972058f0e07644c719a6441f96bdb0b4d6
Jace B <jace.e.blackwell-1@ou.edu>
2023-09-12 16:21:04 -0500

    Finished step 1

--- assignment3.cpp
+++ assignment3.cpp
@@ -1,1 +1,14 @@
-//
+#include <vector>
+#include <iostream>
+using namespace std;
+
+
+int add(vector<int> first)
+{ 
+    int total = 0;
+    for (int i = 0; i < first.size(); ++i)
+    { 
+        total += first.at(i);
+    }
+    return total;
+}
